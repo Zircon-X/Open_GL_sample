a9b41fd401636ddf0090b6f8b9c4c8e649ad6bcf
Zircon-X <zachnorris526@gmail.com>
2016-11-11 17:44:00 -0500

    Updates and Changes

--- main.cpp
+++ main.cpp
@@ -5,20 +5,25 @@
 //INCLUDING GLFW
 #include <GLFW/glfw3.h>
 
-const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 1024, HEIGHT = 768;
 
 const GLchar* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 position;\n"
+	"layout (location = 1) in vec3 color;\n"
+	"out vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"gl_Position = vec4(position, 1.0);\n"
+	"ourColor = color;\n"
+	//"vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);\n"
 	"}\0";
 
 const GLchar* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 color;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"color = vec4(ourColor, 1.0f);\n"
 	"}\n\0";
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
@@ -31,6 +36,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 int main()
 {
+	//------------------------------------------------------//
+	//Window setup											//
+	//------------------------------------------------------//
 	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
 	//Initialize GLFW
 	glfwInit();
@@ -61,10 +69,12 @@ int main()
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
+	//------------------------------------------------------//
+	//End Window setup										//
+	//------------------------------------------------------//
 
 	GLint success;
 	GLchar infoLog[512];
-
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -96,18 +106,33 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
 	};
-	GLuint VBO, VAO;
+	//GLuint indices[] = {
+	//	0, 1, 3,
+	//	1, 2, 3
+	//};
+	//create unsigned GL integers 
+	GLuint VBO, VAO; //EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -120,7 +145,13 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
+		GLfloat timeValue = glfwGetTime();
+		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+		GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
+		//wireframe mode
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 		//swap screen buffers
@@ -128,6 +159,7 @@ int main()
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
 	//Terminate GLFW, clearing up any resources used by GLFW
 	glfwTerminate();
 	return 0;
